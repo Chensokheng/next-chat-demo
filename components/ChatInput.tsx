954b6f52bc30c459b8b7701cb0d8f4a7c3dfd767
dailydemo <dailywebcodingdemo@gmail.com>
2024-01-04 01:52:06 +0900

    feat: add message

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -3,13 +3,37 @@ import React from "react";
 import { Input } from "./ui/input";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@/lib/store/user";
+import { Imessage, useMessage } from "@/lib/store/messages";
 
 export default function ChatInput() {
+	const user = useUser((state) => state.user);
+	const addMessage = useMessage((state) => state.addMessage);
 	const supabase = supabaseBrowser();
 	const handleSendMessage = async (text: string) => {
-		const { error } = await supabase.from("messages").insert({ text });
-		if (error) {
-			toast.error(error.message);
+		if (text.trim()) {
+			const newMessage = {
+				id: uuidv4(),
+				text,
+				send_by: user?.id,
+				is_edit: false,
+				created_at: new Date().toISOString(),
+				users: {
+					id: user?.id,
+					avatar_url: user?.user_metadata.avatar_url,
+					created_at: new Date().toISOString(),
+					display_name: user?.user_metadata.user_name,
+				},
+			};
+			addMessage(newMessage as Imessage);
+
+			const { error } = await supabase.from("messages").insert({ text });
+			if (error) {
+				toast.error(error.message);
+			}
+		} else {
+			toast.error("Message can not be empty!!");
 		}
 	};
 
--- lib/store/messages.ts
+++ lib/store/messages.ts
@@ -17,8 +17,11 @@ export type Imessage = {
 
 interface MessageState {
 	messages: Imessage[];
+	addMessage: (message: Imessage) => void;
 }
 
 export const useMessage = create<MessageState>()((set) => ({
 	messages: [],
+	addMessage: (message) =>
+		set((state) => ({ messages: [...state.messages, message] })),
 }));
