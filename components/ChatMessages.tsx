070eb4f06c2afd9b5d8f610be7022e7c48796d06
dailydemo <dailywebcodingdemo@gmail.com>
2024-01-05 00:52:29 +0900

    feat: limit init fetch

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -2,18 +2,21 @@ import React, { Suspense } from "react";
 import ListMessages from "./ListMessages";
 import { supabaseServer } from "@/lib/supabase/server";
 import InitMessages from "@/lib/store/InitMessages";
+import { LIMIT_MESSAGE } from "@/lib/constant";
 
 export default async function ChatMessages() {
 	const supabase = supabaseServer();
 
-	const { data } = await supabase.from("messages").select("*,users(*)");
-
-	console.log(data);
+	const { data } = await supabase
+		.from("messages")
+		.select("*,users(*)")
+		.range(0, LIMIT_MESSAGE)
+		.order("created_at", { ascending: false });
 
 	return (
 		<Suspense fallback={"loading.."}>
 			<ListMessages />
-			<InitMessages messages={data || []} />
+			<InitMessages messages={data?.reverse() || []} />
 		</Suspense>
 	);
 }
