89ef1435820efc3f65ef7ba90857846baa9380d1
dailydemo <dailywebcodingdemo@gmail.com>
2024-01-05 01:33:03 +0900

    feat: presence

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
+import ChatPresence from "./ChatPresence";
 
 export default function ChatHeader({ user }: { user: User | undefined }) {
 	const router = useRouter();
@@ -29,10 +30,7 @@ export default function ChatHeader({ user }: { user: User | undefined }) {
 			<div className="p-5 border-b flex items-center justify-between h-full">
 				<div>
 					<h1 className="text-xl font-bold">Daily Chat</h1>
-					<div className="flex items-center gap-1">
-						<div className="h-4 w-4 bg-green-500 rounded-full animate-pulse"></div>
-						<h1 className="text-sm text-gray-400">2 onlines</h1>
-					</div>
+					<ChatPresence />
 				</div>
 				{user ? (
 					<Button onClick={handleLogout}>Logout</Button>
---
+++ components/ChatPresence.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useUser } from "@/lib/store/user";
+import { supabaseBrowser } from "@/lib/supabase/browser";
+import React, { useEffect, useState } from "react";
+
+export default function ChatPresence() {
+	const user = useUser((state) => state.user);
+	const supabase = supabaseBrowser();
+	const [onlineUsers, setOnlineUsers] = useState(0);
+
+	useEffect(() => {
+		const channel = supabase.channel("room1");
+		channel
+			.on("presence", { event: "sync" }, () => {
+				console.log("Synced presence state: ", channel.presenceState());
+				const userIds = [];
+				for (const id in channel.presenceState()) {
+					// @ts-ignore
+					userIds.push(channel.presenceState()[id][0].user_id);
+				}
+				setOnlineUsers([...new Set(userIds)].length);
+			})
+			.subscribe(async (status) => {
+				if (status === "SUBSCRIBED") {
+					await channel.track({
+						online_at: new Date().toISOString(),
+						user_id: user?.id,
+					});
+				}
+			});
+	}, [user]);
+
+	if (!user) {
+		return <div className=" h-3 w-1"></div>;
+	}
+
+	return (
+		<div className="flex items-center gap-1">
+			<div className="h-4 w-4 bg-green-500 rounded-full animate-pulse"></div>
+			<h1 className="text-sm text-gray-400">{onlineUsers} onlines</h1>
+		</div>
+	);
+}
