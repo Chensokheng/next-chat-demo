25cf7b9d057e0211fcaab03089ad4b3799cf13ca
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2024-01-09 16:50:36 +0700

    feat: sending id

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -1,5 +1,8 @@
 export async function POST(req: Request) {
 	console.log(req.body, "====");
+	const data = await req.json();
+
+	console.log(data, "-----");
 
 	await fetch(
 		"https://discord.com/api/webhooks/1194202014377320509/rL1B15fdeZgsaNXK_LIGfBqiB-N52rkIjjEFS4spUMJh8ra6-bpY-O24qPN14KgUZqbn",
@@ -10,7 +13,7 @@ export async function POST(req: Request) {
 			},
 			body: JSON.stringify({
 				//@ts-ignore
-				content: "new order + " + req.body?.record?.id,
+				content: "new order + " + data.record.id,
 			}),
 		}
 	);
