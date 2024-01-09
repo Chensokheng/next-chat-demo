6cbbd202cb89aa358e993cb1b3f729ff51c2635a
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2024-01-09 17:03:40 +0700

    feat: protect

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -1,22 +1,21 @@
 export async function POST(req: Request) {
-	console.log(req.body, "====");
-	const data = await req.json();
-
-	console.log(data, "-----");
-
-	await fetch(
-		"https://discord.com/api/webhooks/1194202014377320509/rL1B15fdeZgsaNXK_LIGfBqiB-N52rkIjjEFS4spUMJh8ra6-bpY-O24qPN14KgUZqbn",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				//@ts-ignore
-				content: "new order + " + data.record.id,
-			}),
-		}
-	);
-
-	return Response.json({ result: "ok" });
+	if (req.headers.get("user-agent") === "pg_net/0.7.1") {
+		const data = await req.json();
+		await fetch(
+			"https://discord.com/api/webhooks/1194202014377320509/rL1B15fdeZgsaNXK_LIGfBqiB-N52rkIjjEFS4spUMJh8ra6-bpY-O24qPN14KgUZqbn",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					//@ts-ignore
+					content: "new order + " + data.record.id,
+				}),
+			}
+		);
+		return Response.json({ result: "ok" });
+	} else {
+		return Response.json({ result: "not sending message" });
+	}
 }
